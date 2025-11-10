76d0580141298c656040cc9306915f24bc5def80
Alberto Testa <alberto.testa@post.runi.ac.il>
2025-11-10 20:30:57 +0200

    finished Cheers

--- Cheers.java
+++ Cheers.java
@@ -8,6 +8,32 @@ Great work boaz!
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+
+                String word = args[0];
+                int n = Integer.parseInt(args[1]);
+                String uword = word.toUpperCase();
+
+                int charc = uword.length();
+                int i = 0;
+
+                while (i < charc) {
+                     char ch = uword.charAt(i);
+                     
+                     if (ch=='A' || ch=='E' || ch=='F' || ch=='H' || ch=='I' || ch=='L' || ch=='M' || ch=='N' || ch=='O' || ch=='R' || ch=='S' || ch=='X') {
+                        System.out.println("Give me an "+ch+": "+ch+"!");
+                     } else {
+                        System.out.println("Give me a  "+ch+": "+ch+"!");
+                     }
+                     
+                     i++;
+                }
+	   
+                System.out.println("What does it spell?");
+
+                int j = 0;
+                while (j<n) {
+                        System.out.println(uword+"!!!");
+                        j++;
+                }
         }
 }
--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,22 @@
 public class TimeCalc {
     public static void main(String[] args) {
-        // Replace this comment with your code
+        
+        int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
+		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
+        int minAdd = Integer.parseInt(args[1]);
+
+        int Tmin = (hours*60) + minutes + minAdd;
+        int Thours = Tmin/60;
+
+        int Nhours = Thours % 24;
+        int Nmin = Tmin - (Thours*60);
+
+        String Phours = String.format("%02d", Nhours);
+        String Pmin = String.format("%02d", Nmin);
+        
+         System.out.println(hours+":"+minutes+" "+minAdd);
+         System.out.println(Phours+":"+Pmin);
+        
+       
     }
 }
