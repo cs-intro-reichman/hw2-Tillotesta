4dc0c438f52560ad1df244f12754f4d1f5b555d5
Alberto Testa <alberto.testa@post.runi.ac.il>
2025-11-11 21:00:12 +0200

    finished CalcPi

--- CalcPi.java
+++ CalcPi.java
@@ -1,13 +1,13 @@
 // Computes an approximation of PI.
 public class CalcPi {
-	public static void main(String [] args) { 
+    public static void main(String [] args) { 
 
-		if (args.length == 0) {
+        if (args.length == 0) {
             System.err.println("Error: Please provide the number of terms.");
             return;
         }
-	    
-		 int n;
+        
+        int n;
         try {
             n = Integer.parseInt(args[0]);
         } catch (NumberFormatException e) {
@@ -15,20 +15,19 @@ public class CalcPi {
             return;
         }
 
-		double calc = 0;
-		
-		for (int i = 0; i < n ; i++) {
-			double denominator = (2.0*i + 1);
-			double numerator = (i % 2 == 0) ? 1.0 : -1.0;
+        double calc = 0;
+        
+        for (int i = 0; i < n; i++) {
+            double denominator = (2.0 * i + 1);
+            double numerator = (i % 2 == 0) ? 1.0 : -1.0;
             calc += numerator / denominator;
-		}
+        }
 
-		double pi = calc * 4.0;
+        double pi = calc * 4.0;
 
-		System.out.println("pi according to Java: "+ Math.PI);
-		System.out.flush();
-		System.out.println("pi, approximated:     " + pi);
-		System.out.flush();
-
-	}
+        System.out.println("pi according to Java: " + Math.PI);
+        System.out.flush();
+        System.out.println("pi, approximated:     " + pi);
+        System.out.flush();
+    }
 }
