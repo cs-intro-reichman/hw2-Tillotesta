5446ad95dd2fed71403c06d620d5620a180db1f1
Alberto Testa <alberto.testa@post.runi.ac.il>
2025-11-11 21:46:30 +0200

    finished CalcPi

--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,20 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+
+		int seed = Integer.parseInt(args[0]);
+		String mode = (args[1]);
+
+		if (mode.equals("c")) {
+			System.out.println("Every one of the first " + seed + " hailstone sequences reached 1.");
+		}
+
+		if (mode.equals("v")) {
+			
+			
+		}
+
+
+
 	}
 }
