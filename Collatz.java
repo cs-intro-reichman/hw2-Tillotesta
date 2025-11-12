e4238aaecad4d4e4fabfeada6722684f312f0e0c
Alberto Testa <alberto.testa@post.runi.ac.il>
2025-11-12 16:45:29 +0200

    finished Collatz

--- Collatz.java
+++ Collatz.java
@@ -2,38 +2,50 @@
 public class Collatz {
 	public static void main(String args[]) {
 
+		// Argument check
+		if (args.length < 2) {
+			System.err.println("Usage: java Collatz <seed> <c|v>");
+			return;
+		}
+
 		int seed = Integer.parseInt(args[0]);
-		String mode = (args[1]);
+		String mode = args[1];
 		int k = seed;
 
 		if (mode.equals("c")) {
 			System.out.println("Every one of the first " + seed + " hailstone sequences reached 1.");
 		}
-		
+
 		if (mode.equals("v")) {
 
 			int i = 1;
 
 			while (i <= k) {
-				int steps = i;
-				System.out.println(i);
-				
-				while (i != 1) {
-				
-					if (i % 2 == 0) {
-						i = i / 2;
+				int n = i;
+				int steps = 1; 
+				System.out.print(n); 
 
-					} else {
-							i = (i*3) + 1;						}
-
-					System.out.println(" "+i);
+				
+				if (n == 1) {
+					n = 4;
+					System.out.print(" " + n);
 					steps++;
 				}
-				System.out.println(i+" "+seed);
+
+				while (n != 1) {
+					if (n % 2 == 0) {
+						n = n / 2;
+					} else {
+						n = (n * 3) + 1;
+					}
+					System.out.print(" " + n);
+					steps++;
+				}
+
+				System.out.println(" (" + steps + ")");
 				i++;
-				System.out.println(" ("+steps+") ");
-			}	
-			
+			}
+
 			System.out.println("Every one of the first " + k + " hailstone sequences reached 1.");
 		}
 	}
