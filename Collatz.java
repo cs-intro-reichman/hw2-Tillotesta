11cec81e2c9ebc24140a13b97625db50469bdaee
Alberto Testa <alberto.testa@post.runi.ac.il>
2025-11-12 11:35:36 +0200

    finished CalcPi

--- Collatz.java
+++ Collatz.java
@@ -4,17 +4,43 @@ public class Collatz {
 
 		int seed = Integer.parseInt(args[0]);
 		String mode = (args[1]);
+		int k = seed;
 
 		if (mode.equals("c")) {
 			System.out.println("Every one of the first " + seed + " hailstone sequences reached 1.");
 		}
+		
 
 		if (mode.equals("v")) {
+
+			for (int s = 1; s <= 1; s++) {
+				StringBuilder sequence = new StringBuilder();
+				int i = 1;
+				while (i <= seed) {
+				
+					while (seed != 1) {
+				
+						if (seed % 2 == 0) {
+							seed = seed / 2;
+
+						} else {
+							seed = (seed*3) + 1;
+						}
+
+					sequence.append(seed).append(" ");	
+				
+					i++;
+					}
+
+					System.out.println(sequence+" ");
+				
+				}	
 			
-			
+					System.out.println("Every one of the first " + k + " hailstone sequences reached 1.");
+			}
 		}
 
-
+		
 
 	}
 }
